0bf4e8a8ccdc02dcc0d6978a8ad82ac07d1e713f
turnturnturn <xogbl@icloud.com>
2016-12-11 17:11:24 -0800

    Актуальная версия супрем

---
+++ supreme/js_type.js
@@ -0,0 +1,21 @@
+var n = 123; // тип number
+n = 12.345; // тип number
+
+var cute = "(◕‿◕✿)",
+var cute = "\"(◕‿◕✿)"
+
+var whyudidthis = '(ಥ﹏ಥ)';
+
+var hacked = true; тип boulean равен true
+var hacked = false; ип boulean равен false
+
+var iknow = null;
+var x;
+
+var code = {}
+var code = {
+  "javascript": true, //тип object содержит две переменные javascript и ruby
+  "ruby": false
+};
+
+typeof console.log
---
+++ supreme/oldschool.js
@@ -0,0 +1,5 @@
+prompt(title, default);
+var result = prompt("whats your name?");
+console.log(promt())
+
+alert("ok");
